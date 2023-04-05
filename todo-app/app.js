c5d01f09d02b918de2c6c3b44b9e1f3fd9b0814a
kupi <72503037+Kupendiran181@users.noreply.github.com>
2023-04-05 19:44:22 +0530

    Add files via upload

---
+++ todo-app/app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const path = require("path");
+const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+
+app.get("/", async (request, response) => {
+  const allTodoItems = await Todo.getTodoItems();
+  if (request.accepts("html")) {
+    response.render("index", { allTodoItems });
+  } else {
+    response.json(allTodoItems);
+  }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/todos", async function (_request, response) {
+  try {
+    const todo = await Todo.gettodoitems();
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addtodoitems(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const deleteTodo = await todo.deletetodoitem({ todo: request.params.id });
+    return response.send(true);
+  } catch (error) {
+    console.log(error);
+    return response.send(false);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+
+
+module.exports = app;
